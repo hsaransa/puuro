0d40a717ea2584acda7b74470ccfb811a4a68a71
Hannu Saransaari <hsaransa@cc.hut.fi>
2010-03-24 15:01:02 +0200

    package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,15 @@
-tar cvfz puuro.tar.gz \
-`find . -iname "*.puuro" -or -iname "*.cpp" -or -iname "*.hpp" -or \
-        -iname "SConstruct" -or -iname "*.c" -or -iname "*.h" -or \
-        -iname "*.html" -or -iname "*.js" -or -iname "*.yy"`
+PKG="puuro-"`date +%y%m%d`
+
+FILES=`find . -iname "*.puuro" -or -iname "*.cpp" -or -iname "*.hpp" -or \
+              -iname "SConstruct" -or -iname "*.c" -or -iname "*.h" -or \
+              -iname "*.html" -or -iname "*.js" -or -iname "*.yy"`
+for i in $FILES
+do
+  mkdir -vp $PKG/`dirname $i` || exit 1
+  cp -v $i $PKG/$i || exit 2
+done
+
+tar cvfz $PKG.tar.gz $PKG || exit 3
+
+rm -fr $PKG
+
